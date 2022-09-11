f71bf3ef8eabfe99cf09854c036253230435765f
GlacialDreamss <danieladetokunbo05@gmail.com>
2022-09-11 16:37:22 +0100

    Comparing the data
    Make subroutines

---
+++ fileCompare.py
@@ -0,0 +1,26 @@
+import os
+import csv
+
+file = open("byteAverage.csv","w",newline='')
+csvWrite = csv.writer(file)
+
+dBytes = 0
+sBytes = 0
+cBytes = 0
+gBytes = 0
+
+for num in range(0,4):
+    print
+def average(): 
+    dBytes += os.stat('dataset/image'+str(num)+'.png').st_size
+    sBytes += os.stat('sobel/image'+str(num)+'.png').st_size
+    cBytes += os.stat('canny/image'+str(num)+'.png').st_size
+    gBytes += os.stat('greyscale/image'+str(num)+'.png').st_size
+
+    cBpercent = (cBytes/dBytes)*100
+    sBpercent = (sBytes/dBytes)*100
+    gBpercent = (gBytes/dBytes)*100
+
+    csvWrite.writerow([str(round(dBytes/(num+1))),str(round(gBytes/(num+1))),str(round(sBytes/(num+1))),str(round(cBytes/(num)+1))])
+
+file.close()
