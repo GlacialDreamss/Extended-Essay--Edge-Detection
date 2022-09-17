b7cfe96566e6f4936bc283ad73ad98d1466a9b3d
GlacialDreamss <danieladetokunbo05@gmail.com>
2022-09-17 13:27:30 +0100

    More Data

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Extended Essay: Edge Detection
  Images are cool
  
- Source: https://docs.opencv.org/4.x/da/d22/tutorial_py_canny.html
  Multiple implementations of edge detection for school
  I like school very much :P
--- fileCompare.py
+++ fileCompare.py
@@ -7,25 +7,22 @@ csvWrite = csv.writer(file)
 dBytes = 0
 sBytes = 0
 cBytes = 0
-gBytes = 0
+gBytes = 0 
 
-def average():
-    dBytes = 0
-    sBytes = 0
-    cBytes = 0
-    gBytes = 0 
+for num in range(0,100):
+    dBytes += os.stat('dataset/image'+str(num)+'.tiff').st_size
+    sBytes += os.stat('sobel/image'+str(num)+'.tiff').st_size
+    cBytes += os.stat('canny/image'+str(num)+'.tiff').st_size
+    gBytes += os.stat('greyscale/image'+str(num)+'.tiff').st_size
 
-    for num in range(0,4):
-        dBytes += os.stat('dataset/image'+str(num)+'.png').st_size
-        sBytes += os.stat('sobel/image'+str(num)+'.png').st_size
-        cBytes += os.stat('canny/image'+str(num)+'.png').st_size
-        gBytes += os.stat('greyscale/image'+str(num)+'.png').st_size
+cBpercent = (cBytes/dBytes)*100
+sBpercent = (sBytes/dBytes)*100
+gBpercent = (gBytes/dBytes)*100
 
-    cBpercent = (cBytes/dBytes)*100
-    sBpercent = (sBytes/dBytes)*100
-    gBpercent = (gBytes/dBytes)*100
+csvWrite.writerow([str(dBytes),str(gBytes),str(sBytes),str(cBytes)]) # Total bytes per test in a category
 
-    csvWrite.writerow([str(round(dBytes/(num+1))),str(round(gBytes/(num+1))),str(round(sBytes/(num+1))),str(round(cBytes/(num)+1))])
+csvWrite.writerow([str(round(dBytes/(num+1))),str(round(gBytes/(num+1))),str(round(sBytes/(num+1))),str(round(cBytes/(num)+1))]) # Average bytes per test in a category
 
+csvWrite.writerow([str(round(cBpercent)),str(round(sBpercent)),str(round(gBpercent))]) # Percentage of bytes leftover per test relatvie to the original dataset
 
 file.close()
